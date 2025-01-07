a153fd80e9858197cc7a72f032f629c8ac0a1018
Dania <vasile.daniela.mariana38@gmail.com>
2025-01-07 19:49:56 +0200

    third commit: services

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {TodosService} from '../services/todos.service';
+import {Todo} from '../model/todo.type';
 
 @Component({
   selector: 'app-todos',
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
+  providers: [TodosService]
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
 
+  ngOnInit(): void {
+    console.log(this.todoService.todoItems);
+    this.todoItems.set(this.todoService.todoItems);
+  }
 }
